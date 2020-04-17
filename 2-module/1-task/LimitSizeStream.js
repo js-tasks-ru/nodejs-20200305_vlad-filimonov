37ffac3a8e36015817c146802258641e289095cc
VladmaxPower <filimonovvladik@gmail.com>
2020-04-17 13:11:05 +0300

    1 task 2 module

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,14 +1,22 @@
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
-
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+    constructor(options) {
+        super(options);
 
-  _transform(chunk, encoding, callback) {
+        this.limit = options.limit;
+        this.size = 0;
+    }
 
-  }
+    _transform(chunk, encoding, callback) {
+        this.size += chunk.length;
+
+        if (this.size > this.limit) {
+            callback(new LimitExceededError());
+        } else {
+            callback(null, chunk);
+        }
+    }
 }
 
 module.exports = LimitSizeStream;
